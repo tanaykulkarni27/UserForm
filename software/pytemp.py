b4938b41f959b188b06c92b1fda06702f21b4525
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2021-04-26 18:50:47 +0530

    Update pytemp.py

--- software/pytemp.py
+++ software/pytemp.py
@@ -1,11 +1,15 @@
 def debug(*args):
-		try:
-			print(*args)
-		except:
-				print(args)
+	try:
+		print(*args)
+	except:
+		print(args)
+def read(typ = str):
+	return typ(input())
+def read_arr(typ):
+	return list(map(typ,input().split()))
 def solve():
 		pass
 t = int(input())
 for i in range(1,t+1):
-		print("Case #{}:".format(i),end=' ')
-		solve()
+	print("Case #{}:".format(i),end=' ')
+	solve()
