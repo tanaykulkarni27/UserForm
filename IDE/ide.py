ed106d1fda2c141327921e7c462022e1a5bec745
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2021-05-13 14:00:18 +0530

    Update ide.py

--- IDE/ide.py
+++ IDE/ide.py
@@ -8,7 +8,7 @@ root.title(" MYIDE")
 def workspace():
 	font_object = Font(family = "Lucida Console",size = 13)
 	tb = font_object.measure('    ')
-	a = Text(root,width = 92,height = 20,bg = "blue",foreground="YELLOW",font = font_object,cursor = 'xterm #FFFFFF',tabs=tb);
+	a = Text(root,width = 92,height = 20,bg = "blue",foreground='yellow',font = font_object,cursor = 'xterm #FFFFFF',tabs=tb);
 	a.pack()
 	return a
 def crater():
@@ -16,12 +16,12 @@ def crater():
 	a.pack(side=LEFT,padx = 0,pady=0)
 	return a
 def get_canvas():
-	cnvs = Canvas(root,width = 1200,height = 700)
+	cnvs = Canvas(root,bg='orange',width = 1200,height = 700)
 	cnvs.pack(side=LEFT,pady=0)
 	#cnvs.configure(bg="blue")
 	return cnvs
 def file_name_label():
-	a = Label(root,text = "File Name")	
+	a = Label(root,text = "File Name",bg='orange')	
 	a.pack()
 	return a
 cnvs = get_canvas();	
@@ -51,13 +51,13 @@ def write_file():
 		
 	fl.close()
 def file_create_button():
-	btn = Button(root,width = 10,text = "Open or Save",command = write_file)
+	btn = Button(root,width = 10,bg='yellow',text = "Open or Save",command = write_file)
 	btn.pack(padx = 10)
 	return btn
 def opn():
 	os.system('./creater')
 def file_formatter_button():
-	btn = Button(root,width = 15,text = 'Open File Creater',command = opn)
+	btn = Button(root,width = 15,bg='yellow',text = 'Open File Creater',command = opn)
 	btn.pack(padx = 10)
 	return btn
 create_file_button = file_create_button()
@@ -78,7 +78,7 @@ def sinp():
 	inf.write(code_input.get("1.0", 'end-1c'))
 	inf.close()
 def add_input_btn():
-	btn = Button(root,width=10,text="save input",command = sinp)
+	btn = Button(root,width=10,bg='yellow',text="save input",command = sinp)
 	btn.pack()
 	return btn
 def run():
@@ -97,21 +97,21 @@ def run():
 			os.system(cmd)		
 	except Exception as e:
 		pass		
-	a = Text(root,width = 43,height = 10,bg = 'blue',wrap = None)
+	a = Text(root,width = 47,height = 10,bg = 'blue',wrap = None)
 	a.configure(foreground = 'white')
 	a.pack(fill = X)
 	cnvs.create_window(840,615,window = a)
 	a.insert(END,output.read())
 def run_f_btn():
-	btn = Button(root,width=10,text="Run File",command = lambda:run())
+	btn = Button(root,width=10,bg='yellow',text="Run File",command = lambda:run())
 	btn.pack()
 	return btn
 save_btn = add_input_btn()
-cnvs.create_window(720,510,window = save_btn)
+cnvs.create_window(750,20,window = save_btn)
 run_btn = run_f_btn()
-cnvs.create_window(820,510,window = run_btn)
-a = Text(root,width = 43,height = 10,bg = 'blue',foreground='white')
+cnvs.create_window(860,20,window = run_btn)
+a = Text(root,width = 45,height = 12,bg = 'blue',foreground='white')
 a.pack()
-cnvs.create_window(840,615,window = a)
+cnvs.create_window(835,589,window = a)
 
 mainloop()
